3484a5693326cd73f5a968d8a0385c681fd9acac
bcright <1256514466@qq.com>
2024-05-06 21:53:47 +0800

    move adjusting limiter from main to decision maker

--- src/decision_maker.py
+++ src/decision_maker.py
@@ -1,9 +1,12 @@
-from src.models.drl_model import DRLModel
+
 
 class DecisionMaker:
-    def __init__(self):
-        self.model = DRLModel()
+    def __init__(self, model, limiter):
+        self.model = model
+        self.limiter = limiter
 
-    def make_decision(self, data):
+    def make_decision_and_adjust_limiter(self, data):
         decision = self.model.predict(data)
-        return decision
+        new_limit = int(decision.item() * 100)  # 根据需要调整这个转换逻辑
+        self.limiter.adjust_threshold(new_limit)
+        return decision, new_limit
--- src/main.py
+++ src/main.py
@@ -4,24 +4,22 @@ from config import DevelopmentConfig
 from models.drl_model import DRLModel
 from monitor import Monitor
 from limiter import Limiter
+from decision_maker import DecisionMaker
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 monitor = Monitor()
 limiter = Limiter(100)  # 每秒最多100个请求
 model = DRLModel.load_model('best_model.pth', state_dim=2, action_dim=1)
+decision_maker = DecisionMaker(model, limiter)
 
 
 @app.route('/predict', methods=['GET'])
 def predict():
     monitor.update_metrics()
     current_state = monitor.get_current_state()
-    prediction = model.predict(current_state)
-    
-    # 假设模型输出是新的请求限制值
-    new_limit = int(prediction.item() * 100)  # 根据需要调整这个转换逻辑
-    limiter.adjust_threshold(new_limit)
-    
+    prediction, new_limit = decision_maker.make_decision_and_adjust_limiter(current_state)
+
     return jsonify({
         'Predicted Action': prediction.item(),
         'New Limit': new_limit
